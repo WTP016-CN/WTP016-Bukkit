35b38a31d861495b23651710d45bbc1c1414d2d8
shaokeyibb@qq.com <shaokeyibb@qq.com>
2020-08-06 19:41:43 +0800

    Upgrade Extensions Main Files.

---
+++ src/main/java/cn/wtp016cn/wtp016test/WTP016test.java
@@ -0,0 +1,17 @@
+package cn.wtp016cn.wtp016test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WTP016test extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
