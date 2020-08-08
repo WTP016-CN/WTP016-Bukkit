e3a420caadc8877f17d74497538575c35eece43b
shaokeyibb@qq.com <shaokeyibb@qq.com>
2020-08-08 13:50:33 +0800

    Update sth

--- src/main/java/cn/wtp016cn/wtp016test/WTP016test.java
+++ src/main/java/cn/wtp016cn/wtp016test/WTP016test.java
@@ -1,18 +1,40 @@
 package cn.wtp016cn.wtp016test;
-
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class WTP016test extends JavaPlugin {
+public final class WTP016test<True> extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Y_Mical 正在 被 龙腾猫跃 夹")
+        getLogger().info("Hello, World!");
+        getLogger().info("This Plugins is open source on Github.");
 
     }
 
     @Override
     public void onDisable() {
-        System.out.println("Y_Mical 正在 被 WTP016 夹 (run");
-        // Plugin shutdown logic
+        getLogger().info("Goodbye, World!");
+        getLogger().info("I want to see you again.");
     }
-}
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("hello")) { //如果玩家输入了/hello则执行如下内容...
+            sender.sendMessage("This is WTP016's beta extensions, version: v0.0.1-Alpha(SNAPSHOT)");
+            return true;
+        } else if (cmd.getName().equalsIgnoreCase("hello2")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("This command only can use by player.");
+            } else {
+                Player player = (Player) sender;
+                sender.sendMessage("This is WTP016's beta extensions. Version: v0.0.1-Alpha");
+            }
+        }
+    }
+
+
+
+
+
